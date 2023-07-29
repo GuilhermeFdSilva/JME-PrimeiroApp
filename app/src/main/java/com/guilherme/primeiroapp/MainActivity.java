88b59f50ae55291c6876d45f8da5d672e8097e71
Guilherme <francaguilherme27@gmail.com>
2023-07-29 13:12:00 -0300

    Finalização projeto - Sortear número aleatório

--- app/src/main/java/com/guilherme/primeiroapp/MainActivity.java
+++ app/src/main/java/com/guilherme/primeiroapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.guilherme.primeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero (View view) {
+        TextView texto = findViewById(R.id.numSort);
+        int numero = new Random().nextInt(11);
+        texto.setText(String.format("Número: %d", numero));
     }
 }
